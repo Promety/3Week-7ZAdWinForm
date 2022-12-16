de0012f935107a416b2657b57dcec10ad77241d4
user <user@WIN-349MCH6R7UR>
2022-12-16 18:08:39 +0300

    Добавьте файлы проекта.

---
+++ 7ZAdWinForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7ZAdWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static bool F(string s)
+        {
+            for (int i = 0; i < s.Length - 1; i++)
+                if (s[i] == s[i + 1])
+                    return true;
+
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text + Environment.NewLine;
+            textBox2.Text += F(s) + Environment.NewLine;
+        }
+    }
+}
+
---
+++ 7ZAdWinForms/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7ZAdWinForms
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string str = Convert.ToString(textBox1.Text);
+            char c = char.Parse(textBox2.Text);
+            textBox3.Text += (Regex.Replace(str, $@"\w*{c}\w*", "", RegexOptions.IgnoreCase));
+            if (!str.Contains(c))
+            {
+                textBox3.Text = null;
+                MessageBox.Show ("В словах нет данного символа", "Error");
+                return;
+            }
+            
+        }
+
+       // если потребуется посмотреть 1 форму перейдите в Program.cs
+    }
+}
